cc344e43b8536c51fff43e7e4853a69f8719a5f4
Prashant Singh <prashantsinghbisht710@gmail.com>
2023-01-18 22:35:01 +0530

    my first changes

---
+++ src/Component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './apply.css';
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='instagram'>InstaGram</div>
+        <div className='search'>Search</div>
+        <div className='login-signup'>
+            <div>login</div>
+            <div>signUp</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Component/Profile.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <div className='profile-container'>
+        <div className="profile">
+            <div className="image">
+                <img src='https://i.pinimg.com/280x280_RS/eb/a6/d7/eba6d76d01681cd9787a67163eb64e09.jpg'></img>
+            </div>
+        </div>
+        <div className="profile-info">
+            <h1>Leanne Graham</h1>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Profile
